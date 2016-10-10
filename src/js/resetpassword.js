ccc62d0c32301c9addf9b1672e7f96052c9d3ec2
Erik Andersson <erikandersson91_329@hotmail.com>
2016-10-10 10:01:45 +0200

    Fixed i19 reset password

---
+++ src/js/resetpassword.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#login").addClass("current-view");
+
+    function el(elementType) {
+        return $(document.createElement(elementType));
+    }
+
+    function clearComplaints() {
+        $(".complaint").remove()
+    }
+
+    function complainSomeMore(where, what) {
+        $(where).append(el("div").addClass("complaint").text(what));
+    }
+
+    window.api.ajax("GET", window.api.host + "/v1/account/login")
+    .done(ok => window.location = "/profile.html")
+
+    $("#confirm-btn").on("click", function(evt) {
+        if($("#password").val() === $("#password2").val()){
+          var req = window.user.resetpassword($("#password").val())
+
+          req.done(xhr => window.location = "profile.html")
+
+          req.fail(xhr => {
+              complainSomeMore("#password2-wrapper", xhr.responseText)
+              $("#password").val("")
+              $("#password2").val("")
+          })
+
+        } else {
+          clearComplaints()
+          complainSomeMore("#password2-wrapper","The passwords do not match");
+        }
+    });
+});
