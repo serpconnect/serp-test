3ea19734871ab7d882f5afa0eaa029f5d5fc7c9f
Jamie Caprani <ja8674ca-s@student.lu.se>
2016-12-01 14:09:07 +0100

    Refactored modal code on profile page types

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,85 +1,149 @@
-(function (CTX) {
+$(function (){
 
-	/**
-	 * window.modal(el('div.modal-header-title', ['title']),
-	 * 				constructEntry(e))
-	 * 			.cancel('close')
-	 * 			.show()
-	 */
+	var modals = window.modals = {}
 
-	var modal = function (header, body, footer) {
-		this.div = el('div.modal', [el('div', [
-			header, 
-			el("div.modal-divider"),
-			body, 
-			el("div.modal-divider"),
-			footer // might be undefined but won't be added in that case
-		])])
+// Modal Template
+	// var modalObject = {  
+ //            desc: "create new collection",
+ //            message: "",
+ //            //single string message that goes above input boxes
+ //            input: [['input0','text','e.g new password'],['input0','text','e.g old password']],
+ //            //[textbox names, types, placeholder] //else put '[]'
+ //            //automatically takes input[0] as first parameter for method passed in.. etc
+ //            btnText: "Create"
+ //            //text on button
+ //        };
+ //        // Create a new collection
+ 	modals.confirmPopUp = function(desc, method){
+ 		var closeBtn = el('div.close-btn', [''])
+ 		var confirmBtn = el('button#confirm.btn', ['confirm'])
+ 		var cancel = el('button.btn', ['cancel'])
+ 		
+ 		var modal = el('div.confirm', [el('div', [
+	        	closeBtn,
+	            el("div.confirm-header-title", [desc]),
+	            //name of modal
+	            el("div#bottom-divider.confirm-divider"),
+	            confirmBtn, cancel
+	        ]) ])
 
-		// The X in the top-right corner
-		var closeBtn = el('div.close-btn', [''])
-		this.div.firstChild.appendChild(closeBtn)
-		closeBtn.addEventListener('click', destroy, false)
-	}
+ 			cancel.addEventListener('click', (evt) => {
+	            document.body.removeChild(modal)
+	        }, false)
 
-    modal.prototype.toggleButtonState = function() {
-        var btns = this.div.querySelectorAll('.btn')
-        for (var i = 0; i < btns.length; i++) {
-            btns[i].classList.toggle('submit-disabled')
-            if (btns[i].getAttribute('disabled'))
-                btns[i].removeAttribute('disabled')
-            else
-                btns[i].setAttribute('disabled', true)
-        }
-    }
+	        confirmBtn.addEventListener('click', (evt) => {
+		      	method.apply({modal} )
+	        })
 
-	modal.prototype.confirm = function (name, cb) {
-		var btn = el('button.btn', [name])
+	        function destroy() {
+				document.body.removeChild(modal)
+			}
 
-		this.div.firstChild.appendChild(btn)
+	       	closeBtn.addEventListener('click', destroy, false)
 
-		btn.addEventListener('click', cb, false)
-		return this
-	}
+			// Remove active modal when pressing ESC
+		    document.addEventListener('keydown', (evt) => {
+		    	if (evt.keyCode === 27) {
+		    		var modal = document.querySelector('.confirm')
+		    		if (!modal) return
+		    		document.body.removeChild(modal)
+		    	}
+		    }, false)
 
-	modal.prototype.cancel = function (name, cb) {
-		var btn = el('button.btn', [name])
+	        document.body.appendChild(modal)
 
-		this.div.firstChild.appendChild(btn)
+	        if(modal.querySelector('input')){
+	        	focus()
+	        }	
+	 }
 
-		if (cb)
-			btn.addEventListener('click', cb, false)
-		else
-			btn.addEventListener('click', destroy, false)
 
-		return this
-	}
 
-	modal.prototype.show = function () {
-		document.body.appendChild(this.div)
-		return this
-	}
+	modals.optionsModal = function(obj, method) {
+	       
+	        var parameters =[]
+	        var inputboxes =[]
+	        var message = [el("div.modal-sub-item", [obj.message])]
+	        var closeBtn = el('div.close-btn', [''])
+	        // The X in the top-right corner
 
-	function destroy() {
-		document.body.removeChild(document.querySelector('div.modal'))
-	}
+	        for(i=0;i<obj.input.length;i++){
+	    			inputboxes[i] =
+	    			[el("input#"+"input"+i+".modal-input-box", {
+		                placeholder: obj.input[i][2],
+		                type: obj.input[i][1],
+		                name: obj.input[i][0]
+	        		})]
+	        		// parameters[i] = $('#input'+i)
+	    		}
+	    	// console.log(parameters)
+	        var button1 = el('button.btn', [obj.btnText])
+	        //creates optional button
+	        var cancel = el('button#cancel.btn', ['cancel'])
+	          //cancel button is a standard feature
+	        var modal = el('div.modal', [el('div', [
+	        	closeBtn,
+	            el("div.modal-header-title", [obj.desc]),
+	            //name of modal
 
-	// Remove active modal when pressing ESC
-    document.addEventListener('keydown', (evt) => {
-    	if (evt.keyCode === 27) {
-    		var modal = document.querySelector('.modal')
-    		if (!modal) return
-    		document.body.removeChild(modal)
-    	}
-    }, false)
+	            el("div.modal-divider"),
+	            message,
+		        inputboxes,
+	            
+	            el("div#bottom-divider.modal-divider"),
+	            button1, cancel
+	        ]) ])
 
-	// Remove active modal when clicking outside modal
-    window.addEventListener('load', () => {
-	    document.body.addEventListener('click', (evt) => {
-	    	if (evt.target.className === "modal")
-	    		document.body.removeChild(evt.target)
-	    })
-	}, false)
-	CTX.modal = modal	
+	        cancel.addEventListener('click', (evt) => {
+	            document.body.removeChild(modal)
+	        }, false)
 
-})(window);
+	        button1.addEventListener('click', (evt) => {
+		      	var args = []
+		      	for(i=0;i<obj.input.length;i++){
+		    			args[i] = $('#input'+i)[0].value
+		    		}
+		    	//maybe apply toggleButtonState()??
+		      	method.apply({modal}, args)
+		      	//put parameters into method
+	        })
+
+	        function destroy() {
+				document.body.removeChild(modal)
+			}
+
+	       	closeBtn.addEventListener('click', destroy, false)
+
+			// Remove active modal when pressing ESC
+		    document.addEventListener('keydown', (evt) => {
+		    	if (evt.keyCode === 27) {
+		    		var modal = document.querySelector('.modal')
+		    		if (!modal) return
+		    		document.body.removeChild(modal)
+		    	}
+		    }, false)
+
+	        document.body.appendChild(modal)
+
+	        if(modal.querySelector('input')){
+	        	focus()
+	        }	
+	 }
+})
+
+// Remove active modal when clicking outside modal
+window.addEventListener('load', () => {
+		    document.body.addEventListener('click', (evt) => {
+		    	if (evt.target.className === "modal")
+		    		document.body.removeChild(evt.target)
+		    })
+}, false)
+
+// Remove confirmation pop up when clicking outside box
+window.addEventListener('load', () => {
+		    document.body.addEventListener('click', (evt) => {
+		    	if (evt.target.className === "confirm")
+		    		document.body.removeChild(evt.target)
+		    })
+}, false)
+	
