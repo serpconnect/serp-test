5f2481719de4e5c5a3bd62061bb61b2f8ee6fc20
Axel Smeets <dat12asm@student.lu.se>
2016-12-16 14:26:26 +0100

    Add leave button back to collection page

--- src/js/collection.js
+++ src/js/collection.js
@@ -6,7 +6,7 @@ $(function () {
 	if (window.location.hash.length === 0)
 		toProfilePage()
 
-	// http://serpconnect.cs.lth.se/collection.html#33
+	// http://serpconnect.cs.lth.se/collection.html#33 => 33
 	var cID = window.location.hash.substring(1)
 
 	function updateLink(el) {
@@ -22,31 +22,40 @@ $(function () {
 		}))
 		.fail(toProfilePage)
 
-	window.api.ajax("GET", window.api.host + "/v1/collection/" + cID + "/stats")
+	window.api.v1.collection.stats(cID)
 		.done(setupStats)
 		.fail(toProfilePage)
 
 	function setupName(collection) {
-		var elName = $('#name').text(collection.name)
-		var elID = $('#id').text(collection.id)
+		$('#name').text(collection.name)
+		$('#id').text(collection.id)
 	}
 
+	/* X member(s), Y entr(y|ies) */
+    function formatStats(members, entries) {
+        var mems = members === 1 ? "member" : "members"
+        var entr = entries === 1 ? "entry" : "entries"
+
+        return `${members} ${mems}, ${entries} ${entr}`
+    }
+
 	function setupStats(stats) {
-		var elStats = $('.collection-stats').text(
-			`${stats.members} user${stats.members > 1 ? 's' : ''}, ${stats.entries} entr${stats.entries > 1 ? 'ies' : 'y'}`)
+		$('.collection-stats').text(formatStats(stats.members, stats.entries))
 	}
 
 	function leave() {
         var confirm = el('button.btn', ['leave'])
         var cancel = el('button.btn', ['cancel'])
 
-        var modal = el('div.modal', [el('div', [
-            el("div.modal-header-title", [$('#name').text()]),
-            el("div.modal-divider"),
-            el("div.modal-sub-item", ['Leave collection?']),
-            el("div.modal-divider"),
-            confirm, cancel
-        ])])
+        var modal = el('div.modal', [
+			el('div', [
+				el("div.modal-header-title", [$('#name').text()]),
+				el("div.modal-divider"),
+				el("div.modal-sub-item", ['Leave collection?']),
+				el("div.modal-divider"),
+				confirm, cancel
+        	])
+		])
 
         cancel.addEventListener('click', (evt) => {
             document.body.removeChild(modal)
@@ -57,12 +66,11 @@ $(function () {
             cancel.classList.add('submit-disabled')
             confirm.setAttribute('disabled', true)
             cancel.setAttribute('disabled', true)
-            window.api.ajax("POST", window.api.host + "/v1/collection/" + cID + "/leave")
-            	.always(toProfilePage)
+            window.api.v1.collection.leave(cID).always(toProfilePage)
         })
 
         document.body.appendChild(modal)
     }
 
-	document.getElementById('logout').addEventListener('click', leave, false)
+	document.getElementById('leave').addEventListener('click', leave, false)
 })
