c18c08ab971a448a51ebf91f652116c9fc01abed
Axel Smeets <dat12asm@student.lu.se>
2016-12-19 16:08:27 +0100

    Optimize entries.js

--- src/js/entries.js
+++ src/js/entries.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             api.v1.entry.taxonomy(entry.id).done(taxonomy => {
                 entries.push(entry)
                 entry.serpClassification = taxonomy
-                insertIntoTable(entry)
+                insertIntoTable(entry, entries.length - 1)
             })
         })
     }).fail(xhr => {
@@ -35,106 +35,103 @@ $(document).ready(function() {
             .fail(xhr => window.alert(xhr.responseText))
     }
     
-    function createCell(subfacet, alternating) {
-        return el(`td${classification[subfacet.toUpperCase()] ? '.filled-cell' : ''}${alternating ? '.alternating-group' : ''}`)
+    function createCell(taxonomy, subfacet, alternating) {
+        var filled = taxonomy[subfacet.toUpperCase()] ? '.filled-cell' : ''
+        var alt = alternating ? '.alternating-group' : ''
+        return el(`td${filled}${alt}`)
     }
 
-    function insertIntoTable(entry, position) {
-        var classification = entry.serpClassification
+    function acceptEntryCallback(evt) {
+        commit(Number(this.dataset.entryNumber), acceptEntry)
+    }
+
+    function rejectEntryCallback(evt) {
+        commit(Number(this.dataset.entryNumber), rejectEntry)
+    }
+
+    function entryButton(name, style, entryNumber, fn) {
+        var btn = el(`button.${style}`, {
+            'data-entry-number': entryNumber
+        }, [name]);
+        btn.addEventListener("click", fn, false);
+        return btn;
+    }
+
+    var maxLength = 35;
+    function insertIntoTable(entry, entryNumber) {
+        var entryTitle = entry["description"] || entry["reference"];
+        if (entryTitle.length > maxLength)
+            entryTitle = entryTitle.substring(0, maxLength - 3) + "..." 
+
+        var title = el('td', [entryTitle])
+        title.addEventListener('click', buildModalView, false)
 
         // let's build the table row for this entry
-        var maxLength = 35;
-        var entryTitle = entry["description"] || entry["reference"];
-        entryTitle = entryTitle.length > maxLength ?
-            entryTitle.substring(0, maxLength - 3) + "..." :
-            entryTitle.substring(0, maxLength);
+        var row = el('tr', {'data-entry-number': entryNumber }, [
+            title,
 
-        var acceptBtn = el("button.entries-accept-btn", ["accept"]);
-        acceptBtn.addEventListener("click", function(evt) {
-            var entryNumber = this.parentNode.children[0].dataset.entryNumber
-            commit(entryNumber, acceptEntry)
-        }, false);
-
-        var rejectBtn = el("button.entries-reject-btn", ["reject"]);
-        rejectBtn.addEventListener("click", function(evt) {
-            var entryNumber = this.parentNode.children[0].dataset.entryNumber
-            commit(entryNumber, rejectEntry)
-        }, false);
-
-        var row = el('tr', [
-            el('td', {'data-entry-number': entries.indexOf(entry) }, [entryTitle]),
-
-            createCell("intervention", true),
+            createCell(entry.serpClassification, "intervention", true),
 
             // effect
-            createCell("solving"),
-            createCell("adapting"),
-            createCell("assessing"),
-            createCell("improving"),
+            createCell(entry.serpClassification, "solving"),
+            createCell(entry.serpClassification, "adapting"),
+            createCell(entry.serpClassification, "assessing"),
+            createCell(entry.serpClassification, "improving"),
 
             // scope
-            createCell("planning", true),
-            createCell("design", true),
-            createCell("execution", true),
-            createCell("analysis", true),
+            createCell(entry.serpClassification, "planning", true),
+            createCell(entry.serpClassification, "design", true),
+            createCell(entry.serpClassification, "execution", true),
+            createCell(entry.serpClassification, "analysis", true),
 
             // context
-            createCell("people"),
-            createCell("information"),
-            createCell("sut"),
-            createCell("other"),
+            createCell(entry.serpClassification, "people"),
+            createCell(entry.serpClassification, "information"),
+            createCell(entry.serpClassification, "sut"),
+            createCell(entry.serpClassification, "other"),
 
-            acceptBtn,
-            rejectBtn
+            el('td', [entryButton('accept', 'entries-accept-btn', entryNumber, acceptEntryCallback)]),
+            el('td', [entryButton('reject', 'entries-reject-btn', entryNumber, rejectEntryCallback)])
         ])
 
         document.getElementById("table-body").appendChild(row);
-
-        $("td:first-child").on("click", function(evt) {
-            var entryNumber = $(this).data("entry-number");
-            var entry = entries[entryNumber];
-            buildModalView(entry, entryNumber);
-        });
     }
 
-    // convenience function for capitalizing sentences
-    String.prototype.capitalize = function() {
-        return this.charAt(0).toUpperCase() + this.slice(1);
-    }
-
-    function buildModalView(entry, entryNumber) {
-        var acceptBtn = el("button.btn", ["accept"]);
-        acceptBtn.addEventListener("click", function(evt) {
-            commit(entryNumber, acceptEntry)
-        }, false);
-
-        var rejectBtn = el("button.btn", ["reject"]);
-        rejectBtn.addEventListener("click", function(evt) {
-            commit(entryNumber, rejectEntry)
-        }, false);
+    function buildModalView(evt) {
+        var entryNumber = Number(this.parentNode.dataset.entryNumber);
+        var entry = entries[entryNumber];
 
         var modalOpt = {
-            button: [acceptBtn, rejectBtn]
+            button: [
+                entryButton('accept', 'btn', entryNumber, acceptEntryCallback),
+                entryButton('reject', 'btn', entryNumber, rejectEntryCallback)
+            ]
         }
 
         modals.entryModal(entry, entry.serpClassification, modalOpt)
     }
 
     function removeEntry(entryNumber) {
-        var num = Number(entryNumber)
-        
-        // remove the entry from the table
-        $("#table-body tr:nth-child(" + (num + 1) + ")").remove();
-
-        // remove from our internal array
-        entries.splice(num, 1);
+        var table = document.getElementById("table-body")
 
         // update data attribute of the table cells after the removed entry
-        var cells = document.querySelectorAll("td:first-child")
-        for (var i = num; i < cells.length; i++) {
-            console.log(i, cells[i], cells[i].dataset)
-            cells[i].dataset.entryNumber = cells[i].dataset.entryNumber - 1
+        for (var i = entryNumber + 1; i < table.children.length; i++) {
+            var row = table.children[i]
+            var num = Number(row.dataset.entryNumber) - 1
+
+            row.dataset.entryNumber = num
+
+            var btns = row.querySelectorAll('button')
+            for (var j = 0; j < btns.length; j++)
+                btns[i].dataset.entryNumber = num
         }
+
+        // remove the entry from the table, entryNumber+1 since nth-child starts from 1
+        var row = table.children[entryNumber]
+        row.parentNode.removeChild(row)
+
+        // remove from our internal array
+        entries.splice(entryNumber, 1);        
     }
 
 });
