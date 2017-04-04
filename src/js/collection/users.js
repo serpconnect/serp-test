a165ee539c711be9ff84e5e21107f1eb01d993a3
Axel Smeets <dat12asm@student.lu.se>
2017-04-04 15:14:29 +0200

    Add kick user btn to collection/users page

--- src/js/collection/users.js
+++ src/js/collection/users.js
@@ -2,6 +2,7 @@ $(function () {
 	var toProfilePage = () => window.location = "/profile.html"
 	var collectionName;
 	var friends =[]
+	var userEmail = "undefined"
 	window.api.v1.account.collections()
 		.done(collections => collections.forEach(collection => {
 			if (collection.id === Number(cID)){
@@ -67,7 +68,40 @@ $(function () {
                 })
     })
 
+	$('#kick').click(evt => {
+		api.v1.collection.members(cID, "all").done(members => {
+			var emails = members.map(user => user.email)
+			emails.splice(emails.indexOf(userEmail), 1)
+
+			var kickUserModal = {
+				desc: "Kick user from " + collectionName,
+				message: "",
+				list: emails,
+				input: [['input0', 'email', 'user email']],
+				btnText: "Kick"
+			}
+
+			//fix alternatives so only people in the collection shows up
+			window.modals.fuzzyModal(kickUserModal, function (email) {
+				window.modals.confirmKickPopUp(`Are you sure you want to kick ${email}?`, () => {
+					api.v1.collection.kick(email, id)
+						.done(ok => cleanup(this.modal))
+						.fail(xhr => complain(xhr.responseText))
+				})
+
+			})
+
+
+			new Awesomplete('#input0', {
+				list: emails,
+				filter: ausomplete.autocompleteFilter,
+				replace: ausomplete.autocompleteUpdate
+			})
+		})
+	})
+
     function setup(self) {
+		userEmail = self.email
     	window.api.v1.account.friends(self.email).done(data => friends = data)
     }
 
