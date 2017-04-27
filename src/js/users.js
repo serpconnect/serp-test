8443926b203348c3ebaecd088a4baf8d95a028ea
Axel Smeets <dat12asm@student.lu.se>
2017-04-27 16:50:46 +0200

    Load level descriptions from jade

--- src/js/users.js
+++ src/js/users.js
@@ -2,23 +2,13 @@ $(document).ready(function() {
     $("#login").text("profile");
     $("#login").addClass("current-view");
 
-    var levelDescriptions = {
-        'registered': "A registered user is an account that has clicked signup " +
-                    "but hasn't clicked the confirmation link in the email. " +
-                    "Such an account cannot be trusted since they could have " +
-                    "entered anyone's email address. They _cannot_ submit new " +
-                    "entries, only create new collections.",
-        'user': "A user is simply an account that has confirmed " +
-                "the signup email, proving that they have ownership" +
-                " of it. Submitted entries must be approved by an " +
-                "admin before they appear in the system.",
-        'verified': "A verified user may submit entries to the system " +
-                    "without going through the approval process.",
-        'admin': "An admin can freely promote/demote all accounts" +
-                ", accept/reject pending entries, remove and edit " +
-                "existing entries. Make sure this" +
-                " account is trusted before proceeding."
-    }
+    var descriptions = Array.from(document.querySelectorAll('.description-field'))
+        .map(el => {
+            var lvl =  el.querySelector('.level').textContent
+            var dsc = el.querySelector('.description').textContent
+            return { [lvl]: dsc }
+        })
+    var levelDescriptions = Object.assign({}, ...descriptions)
 
     user.invites().done(showInvites)
     //check if invites exist and display number above invitations tab on profile page
@@ -37,12 +27,6 @@ $(document).ready(function() {
     var emailsFuzzy = undefined;
     var previousValue = undefined;
 
-    function createLevelOption(level) {
-        return el('option', {
-            'value': level,
-            'title': levelDescriptions[level]
-        }, [level])
-    }
 
     function recordPreviousValue(evt) {
         previousValue = this.value
@@ -51,24 +35,32 @@ $(document).ready(function() {
         if (previousValue === this.value) return
         buildModalView($(this), this.dataset.email, previousValue, this.value);
     }
+    function createLevelOption(level) {
+        return el('option', {
+            'value': level,
+            'title': levelDescriptions[level]
+        }, [level])
+    }
+    function createLevelSelect(user) {
+        var select = el('select.users-select', {
+                'data-email': user.email,
+            },
+            Object.keys(levelDescriptions).map(createLevelOption)
+        )
+        select.value = user.trust.toLowerCase()
+
+        select.addEventListener('focus', recordPreviousValue, false)
+        select.addEventListener('change', changeUserLevel, false)
+        return select
+    }
 
     window.api.ajax("GET", window.api.host + "/v1/admin/users").done(users => {
         var parent = document.querySelector('.users-area')
 
         users.forEach(user => {
-            var select = el('select.users-select', {
-                    'data-email': user.email,
-                },
-                Object.keys(levelDescriptions).map(createLevelOption)
-            )
-            select.value = user.trust.toLowerCase()
-
-            select.addEventListener('focus', recordPreviousValue, false)
-            select.addEventListener('change', changeUserLevel, false)
-            
             var div = el('div.users-container', [
-                el('span', [user.email]),
-                select
+                el('span', [user.email]), 
+                createLevelSelect(user)
             ])
 
             parent.appendChild(div)
