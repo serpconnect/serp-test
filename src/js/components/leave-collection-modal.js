fc55d4a2dc43d5d5633480653c572f237255e898
Axel Smeets <dat12asm@student.lu.se>
2017-10-11 16:28:03 +0200

    Implement leave collection modal

--- src/js/components/leave-collection-modal.js
+++ src/js/components/leave-collection-modal.js
@@ -1,14 +1,36 @@
 (function (G) {
 
 	G.leaveCollectionModal = function (collectionId, isOwner) {
+		var action = isOwner ? 'Delete' : 'Leave'
+		var confirm = el('button#confirm.btn', [action])
+
+		var modal = el('div#modal.modal.appear', [
+            el('div', [
+                closeButton(),
+                el("div.modal-header-title", [obj.desc]),
+                //name of modal
+
+                el("div.modal-divider"),
+                el("div.modal-sub-item", [
+					`${action} ${title}?`
+				]),
+
+                el("div#bottom-divider.modal-divider"),
+                confirm, window.modals.cancelButton()
+            ])
+        ])
+
 		return new Promise(function (F, R) {
-			var title = `#${collectionId}`
-			var message = isOwner? `Delete ${title}?`  : `Leave ${title}?`
-			window.modals.confirmPopUp(message, () => {
+			document.body.appendChild(modal)
+			
+			confirm.addEventListener('click', evt => {
+				window.modals.toggleButtonState()
+
 				window.api.v1.collection.leave(collectionId)
 					.then(F)
-					.fail(xhr => window.alert(xhr.responseText))
-			})
+					.catch(xhr => R(xhr.responseText))
+					.always(() => document.body.removeChild(modal))
+			}, false)
 		})
 	}
 
